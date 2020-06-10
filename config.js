d345a51bd62e2277a920da40f7bb67d54ca4796b
Xavier Carty <xaviercarty@gmail.com>
2020-06-10 14:26:54 -0400

    this is it

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = { // this should be const instead if you're using ES6 standards
-REACT_APP_API_KEY:"e2a6a5a7a5msh205f51dc28acb98p1ae29ajsn7662dae6283c"
-}
-
-console.log("This is annoying")
